23ea9a503d9bfe93aae323483ca6dfd780c3603d
R4G3RUNN3R <ragerunner__gr@hotmail.com>
2026-04-16 03:13:57 -0700

    Add AppV2 using rebuilt router stack

---
+++ src/AppV2.tsx
@@ -0,0 +1,49 @@
+import { BrowserRouter, Navigate, useLocation } from "react-router-dom";
+import AppRouterV2 from "./router/indexV2";
+import { AuthProvider, useAuth } from "./state/AuthContext";
+import { PlayerProvider } from "./state/PlayerContext";
+import { EducationProvider } from "./state/EducationContext";
+import { TimerProvider } from "./state/TimerContext";
+import { JobsProvider } from "./state/JobsContext";
+import { ArenaProvider } from "./state/ArenaContext";
+import Ciel from "./components/ciel/Ciel";
+import { BackendStateBridge } from "./components/state/BackendStateBridge";
+
+function AuthGate({ children }: { children: React.ReactNode }) {
+  const { isLoggedIn } = useAuth();
+  const location = useLocation();
+
+  if (location.pathname === "/register" || location.pathname === "/login") {
+    return <>{children}</>;
+  }
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace state={{ redirectedFrom: location.pathname }} />;
+  }
+
+  return <>{children}</>;
+}
+
+export default function AppV2() {
+  return (
+    <AuthProvider>
+      <PlayerProvider>
+        <TimerProvider>
+          <JobsProvider>
+            <ArenaProvider>
+              <EducationProvider>
+                <BackendStateBridge />
+                <BrowserRouter>
+                  <AuthGate>
+                    <AppRouterV2 />
+                    <Ciel />
+                  </AuthGate>
+                </BrowserRouter>
+              </EducationProvider>
+            </ArenaProvider>
+          </JobsProvider>
+        </TimerProvider>
+      </PlayerProvider>
+    </AuthProvider>
+  );
+}
